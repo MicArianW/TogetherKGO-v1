0dce8fbc142339f9518e04142c4e2ac1207721b1
Mickael Arian <mickaelarian@MacBook-Pro.local>
2025-11-26 17:24:22 -0800

    Initial commit - TogetherKGO Food Resources App

---
+++ config.js
@@ -0,0 +1,5 @@
+// Replace the API key below (keep the quotes).
+// You can also leave it empty and set the key via ?key=... but this is simpler.
+window.__CONFIG__ = {
+  GOOGLE_MAPS_API_KEY: "AIzaSyC09RFuBA_GyMLCqFB2NbtgwG3-fbHqNnw"
+};
